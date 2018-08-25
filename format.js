b649a554281bad23a505eb091d1d6cb3b56c045e
nedw <45544004+nedw@users.noreply.github.com>
2018-08-25 19:05:17 +0100

    Move TFL data parsing and HTML formatting into new files

---
+++ format.js
@@ -0,0 +1,183 @@
+const tableStartBorder = '<table border="1">';
+const tableEnd     = '</table>';
+const tableRow     = '<tr>';
+const tableRowEnd  = '</tr>'
+const tableData    = '<td>';
+const tableDataEnd = '</td>';
+
+function formatTableRowOnClick(funcName, funcArg)
+{
+	return '<tr onclick="' + funcName + '(event, \'' + funcArg + '\')">';
+}
+
+//
+// Search result formatting
+//
+
+function displaySearchSelection(info)
+{
+	var s = '<br>' + tableStartBorder;
+	for (var entry of info) {
+		var modeStr = "";
+		var first = true;
+		for (var mode of entry.modes) {
+			if (first) {
+				modeStr = capitalise(mode);
+				first = false;
+			} else {
+				modeStr += ', ' + capitalise(mode);
+			}
+		}
+		if (entry.stopLetter) {
+			modeStr += " (stop " + entry.stopLetter + ")";
+		}
+		var name = entry.name;
+		if (entry.towards)
+			name += "<br>(towards " + entry.towards + ")";
+			s += formatTableRowOnClick("searchOnClick", entry.id) + 
+		     tableData + name + tableDataEnd +
+			 tableData + modeStr + tableDataEnd + tableRowEnd;
+	}
+	s += tableEnd;
+	selectionInfoDiv.innerHTML = s;
+}
+
+//
+// Stop Point formatting
+//
+
+function formatCheckBoxSelectCell()
+{
+	return '<td class="selectClass">' +
+		   '<input type="checkbox" name="selectCheckbox"  onchange="selectOnChange(event)"></input>' +
+		   tableDataEnd;
+}
+
+function formatSelectButton()
+{
+	return '<input type="button" name="selectButton" value="Select" onClick="selectButtonOnClick(event)"/>';
+}
+
+function formatStopPointInfo(info)
+{
+	// "Select" button
+	var s = formatSelectButton();
+	
+	// Table of stop points
+	s += '<br>' + tableStartBorder;
+	for (var stop of info) {
+		s += formatTableRowOnClick("stopPointOnClick", stop.id) + 
+			 tableData + stop.stopName +
+			 tableData;
+		if (stop.lines)
+			s += stop.lines.join(', ');
+		s += tableData;
+		if (stop.dir)
+			s += stop.dir;
+		// Select check box (normally hidden)
+		s += formatCheckBoxSelectCell() + tableRowEnd;
+	}
+	s += tableEnd;
+	return s;
+}
+
+function capitalise(s)
+{
+	if (s && s.length > 0)
+		return s.charAt(0).toUpperCase() + s.substring(1);
+	else
+		return s;
+}
+
+function formatLineIdentifierInfo(obj)
+{
+	var s = '';
+	var first = true;
+	for (var line of obj.lineIdentifier) {
+		if (line[0] != 'n' || displayNightBuses) {
+			if (first) {
+				s = capitalise(line);
+				first = false;
+			} else {
+				s += ', ' + capitalise(line);
+			}
+		}
+	}
+	return s;
+}
+
+function formatLineGroupInfo(arr)
+{
+	//console.log('formatLineGroupInfo: ', arr);
+	var s = '';
+	if (arr.length > 1)
+		alert("formatLineGroupInfo: more than one entry - taking first");
+	obj = arr[0];
+	if (obj.lineIdentifier && obj.lineIdentifier.length > 0) {
+		var arrivalsId;
+		if (obj.naptanIdReference)
+			arrivalsId = obj.naptanIdReference;
+		else
+			arrivalsId = obj.stationAtcoCode ? obj.stationAtcoCode : '';
+		s = formatLineIdentifierInfo(obj);
+	}
+	return { str: s, id: arrivalsId };
+	
+}
+
+function formatTimeToStationStr(t)
+{
+	var min = Math.floor(t/60);
+	var sec = t - (min * 60);
+	var sec10 = Math.floor(sec / 10);
+	sec = sec - (sec10 * 10);
+	return "" + min + ":" + (sec10 ? sec10 * 10 : "00");
+}
+
+function formatBusStopInfo(info)
+{
+	var s = "<p>";
+	if (info.name)
+		s += info.name;
+	if (info.stopLetter)
+		s += " (stop " + info.stopLetter + ")";
+	if (info.lines) {
+		s += "<br>" + info.lines.join(", ");
+	}
+	if (info.towards) {
+		s += " - " + info.towards;
+	}
+	return s;
+}
+
+//
+// Arrivals info formatting
+//
+
+function formatArrivalsInfo(info)
+{
+	if (debug & DEBUG_REQUEST)
+		console.log("formatArrivalsInfo: ", info);
+	var s = "";
+	if (info.length > 0) {
+		s = '<br>' + tableStartBorder;
+		info.sort(function(a,b) { return a.timeToStation - b.timeToStation; });
+		for (var entry of info) {
+			var dest = entry.destinationName ? entry.destinationName : '';
+			s += tableRow +
+				 tableData + entry.lineName + tableDataEnd +
+				 tableData + dest + tableDataEnd +
+				 tableData + formatTimeToStationStr(entry.timeToStation + 0) + tableDataEnd;
+
+			if (entry.modeName && entry.modeName == "tube") {
+				s += tableData + (entry.currentLocation ? entry.currentLocation : "") + tableDataEnd;
+				s += tableData + (entry.platformName ? entry.platformName : "") + tableDataEnd;
+			}
+		}
+		s += tableEnd;
+	} else
+		s = '<p>(No arrivals information)<br>';
+	s += '<input type="button" class="info" id="arrivalsRefresh" onclick="arrivalsRequestOnClick()" value="Submit" />';
+	return s;
+}
+
---
+++ parse.js
@@ -0,0 +1,192 @@
+//
+// Search result parsing
+//
+
+function getLineNames(lines)
+{
+	var names = [];
+	for (var l of lines) {
+		if (l.name) {
+			names.push(capitalise(l.name));
+		}
+	}
+	return names;
+}
+
+function getInfoFromSearchMatches(obj)
+{
+	var ret = [];
+	if (debug & DEBUG_PARSE)
+		console.log("getInfoFromSearchMatches: obj", obj);
+	if (obj.matches) {
+		for (var match of obj.matches) {
+			if (!match.id) {
+				console.log("getInfoFromSearchMatches(): id not in match");
+			} else {
+				var info = {};
+				info.id = match.id;
+				if (match.towards) {
+					info.towards = match.towards;
+				}
+				if (match.name) {
+					info.name = match.name;
+				}
+				if (match.stopLetter) {
+					info.stopLetter = match.stopLetter;
+				}
+				if (match.lines && match.lines.length > 0) {
+					info.lines = getLineNames(match.lines);
+				}
+				if (match.modes && match.modes.length > 0) {
+					info.modes = match.modes;
+				}
+				
+				/* hack - we assume this mismatch means we can use "id" directly as arrivals id */
+				if (match.topMostParentId && match.topMostParentId != match.id)
+					info.idUsable = true;
+				else
+					info.idUsable = false;
+				ret.push(info);
+			}
+		}
+	}
+	if (debug & DEBUG_PARSE)
+		console.log("getInfoFromSearchMatches: return", ret);
+	return ret;
+}
+
+function getAdditionalProperties(obj)
+{
+	var info = {};
+	if (obj.additionalProperties && obj.additionalProperties.length > 0) {
+		for (var prop of obj.additionalProperties) {
+			if (prop.category) {
+				switch (prop.category) {
+				case "Geo":
+					if (prop.key && prop.key == "Zone") {
+						info.zone = prop.value;
+					}
+					break;
+				case "Direction":
+					if (prop.key) {
+						switch (prop.key) {
+						case "CompassPoint":
+							info.direction = prop.value;
+							break;
+						case "Towards":
+							info.towards = prop.value;
+							break;
+						}
+					}
+					break;
+				default:
+					break;
+				}
+			}
+		}
+	}
+	return info;
+}
+
+function getStopPointLeafInfo(obj, parent)
+{
+	var info = {};
+
+	if (obj.lines && obj.lines.length > 0) {
+		if (obj.stopLetter)
+			info.stopName = "Stop " + obj.stopLetter;
+		else
+		if (parent && parent.commonName)
+			info.stopName = parent.commonName;
+		else
+			info.stopName = "";
+
+		var props = getAdditionalProperties(obj);
+		if (props.towards)
+			info.dir = props.towards;
+		else
+		if (props.direction)
+			info.dir = props.direction;
+
+		// TODO - rename "lines" and "ids"
+		info.lines = [];
+		info.ids = [];
+		for (var line of obj.lines) {
+			if (line.name)
+				info.lines.push(line.name);
+			if (line.id)
+				info.ids.push(line.id);
+		}
+		
+		if (obj.stopType == "NaptanMetroPlatform")	// TODO - better way?
+			info.id = obj.stationNaptan;
+		else
+		if (obj.naptanId)
+			info.id = obj.naptanId;
+		else
+		if (obj.id)
+			info.id = obj.id;
+		else
+		if (obj.stationNaptan)
+			info.id = obj.stationNaptan;
+		else
+			info.id = null;			// what to do here if there is no id ?
+	}
+	return info;
+}
+
+function compareEqual(obj1, obj2)
+{
+	for (var n in obj1) {
+		if (!(n in obj2)) {
+			return false;
+		}
+		if (typeof(obj1[n]) == "object") {
+			if (!compareEqual(obj1[n], obj2[n])) {
+				return false;
+			}
+		} else
+		if (obj1[n] != obj2[n]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function isDuplicate(list, obj)
+{
+	for (var i = 0 ; i < list.length; ++i) {
+		if (compareEqual(list[i], obj)) {
+			if (debug & DEBUG_PARSE)
+				console.log("isDuplicate:", obj);
+			return true;
+		}
+	}
+	return false;
+}
+
+function getStopPointInfo(obj)
+{
+	if (debug & DEBUG_PARSE)
+		console.log("getStopPointInfo: obj", obj);
+	var info = [];
+	getStopPointInfo_recurse(obj, null, info);
+	if (debug & DEBUG_PARSE)
+		console.log("getStopPointInfo: return info", info);
+	return info;
+}
+
+function getStopPointInfo_recurse(obj, parent, result)
+{
+	if (obj.children && obj.children.length > 0) {
+		for (var child of obj.children) {
+			getStopPointInfo_recurse(child, obj, result);
+		}
+	} else {
+		var leaf_info = getStopPointLeafInfo(obj, parent);
+		if (leaf_info.id && !isDuplicate(result, leaf_info)) {
+			result.push(leaf_info);
+		}
+	}
+}
+
